963c67f45cc8d7af5a462c15a3877e2a416dac2d
Samuel <revucky2@uniba.sk>
2024-05-13 12:34:07 +0200

    Final version

--- server.ts
+++ server.ts
@@ -1,14 +1,84 @@
 import express, { Request, Response, NextFunction } from 'express';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
-import cors from 'cors';
 dotenv.config();
-import { Client } from 'pg';
+import pkg from 'pg';
+const { Client } = pkg;
 import jwt, { Secret } from 'jsonwebtoken';
 import { error } from 'console';
+import AdminJS from 'adminjs';
+import AdminJSExpress from '@adminjs/express';
+import Connect from 'connect-pg-simple';
+import session from 'express-session';
+import { Adapter, Resource, Database } from '@adminjs/sql';
+import cors from 'cors';
+import validator, { isMobilePhoneLocales } from 'validator';
 
+
+AdminJS.registerAdapter({
+    Database,
+    Resource,
+});
+
+const db = await new Adapter('postgresql', {
+    connectionString: process.env.DATABASE_URL,
+    database: 'babovkejsn'
+  }).init();
+
+const connectionString = process.env.DATABASE_URL;
+const client = new Client({connectionString});
+client.connect();
+
+const authenticate = async (email: string, password: string) => {
+    return await client.query("select role, password from users where email=$1", [email])
+        .then((res) => {
+            if (res.rowCount === 1 && res.rows[0].role === 'admin' && res.rows[0].password === password) {
+                return Promise.resolve({email: email, password: password});
+            }
+            else {
+                return null;
+            }
+        })
+        .catch((err) => {console.log(err); return null;});
+}
+
+const ConnectSession = Connect(session);
+const sessionStore = new ConnectSession({
+    conObject: {
+      connectionString: process.env.DATABASE_URL
+    },
+    tableName: 'session',
+    createTableIfMissing: true,
+});
+
+const admin = new AdminJS({
+    databases: [db],
+});
+admin.watch();
+
+const adminRouter = AdminJSExpress.buildAuthenticatedRouter(
+    admin,
+    {
+      authenticate,
+      cookieName: 'adminjs',
+      cookiePassword: 'sessionsecret',
+    },
+    null,
+    {
+      store: sessionStore,
+      resave: true,
+      saveUninitialized: true,
+      secret: 'sessionsecret',
+      cookie: {
+        httpOnly: process.env.NODE_ENV === 'production',
+        secure: process.env.NODE_ENV === 'production',
+      },
+      name: 'adminjs',
+    }
+);
 
 const app = express();
+app.use(admin.options.rootPath, adminRouter);
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
@@ -17,23 +87,18 @@ app.use(cors({
 }));
 app.listen(process.env.PORT);
 
-const connectionString = process.env.DATABASE_URL;
-const client = new Client({connectionString, ssl: {rejectUnauthorized: false}});
-client.connect();
-
 app.get('/', (req, res) => {
     res.status(200).send("hello");
 })
-
 
 app.post('/api/authenticate', (req, res) => {
 
     // TODO: hash passwords //
     //////////////////////////
 
-    const { mail, password, rememberMe } = req.body;
+    const { email, password, rememberMe } = req.body;
     client
-    .query(`select * from users where mail = $1`, [mail])
+    .query(`select id, email, password from users where email = $1`, [email])
     .then(dbres => {
         if (dbres.rowCount === 0 || dbres.rows[0]['password'] !== password) {
             res.status(200).json({ authenticated: false, message: 'Authentication failed' });
@@ -45,7 +110,7 @@ app.post('/api/authenticate', (req, res) => {
             const token = jwt.sign(user, process.env.SECRET as Secret, { expiresIn: rememberMe ? "30d" : "1h" });
             res
             .status(200)
-            .json({ authenticated: true, role: user.role, message: 'Authentication successful', token: token});
+            .json({ authenticated: true, message: 'Authentication successful', token: token});
         }
     })
     .catch(err => {
@@ -57,10 +122,8 @@ app.post('/api/authenticate', (req, res) => {
 // middleware verification function
 const cookieJwtAuth = (req: Request, res: Response, next: NextFunction) => {
     const token = req.body.token as string;
-    console.log(req.body);
     try {
         const verifiedToken = jwt.verify(token, process.env.SECRET as Secret);
-        console.log(verifiedToken);
         next();
     } 
     catch (err) {
@@ -68,6 +131,68 @@ const cookieJwtAuth = (req: Request, res: Response, next: NextFunction) => {
         res.status(401).json({error: "invalid token"});
     }
 };
+
+app.post('/api/register', async (req, res) => {
+    //console.log(req.body);
+    const {name, lastname, email, phone, password, country, street, city, postalcode } = req.body
+    if (!validator.isAlpha(name, 'sk-SK')) {
+        res.status(400).send();
+        //console.log("Bad name");
+    }
+    else if (!validator.isAlpha(lastname, 'sk-SK')) {
+        res.status(400).send();
+        //console.log("Bad lastname");
+    }
+    else if (!validator.isEmail(email)) {
+        res.status(400).send();
+        //console.log("Bad email");
+    }
+    else if (!validator.isMobilePhone(phone, 'sk-SK')) {
+        res.status(400).send();
+        //console.log("Bad phone");
+    }
+    else if (!validator.matches(password, /^[a-zA-Z0-9!@#$%^&*()_+{}[\]:;'"<>,.?\/\\~-]+$/) ||
+        !validator.isStrongPassword(password)) {
+            res.status(400).send();
+            //console.log("Bad password");
+    }
+    else if (!validator.matches(country, /^Slovensko$/)) {
+        res.status(400).send();
+        //console.log("Bad country");
+    }
+    else if (!validator.isAlphanumeric(street, 'sk-SK', {ignore: " "})) {
+        res.status(400).send();
+        //console.log("Bad street");
+    }
+    else if (!validator.isAlpha(city, 'sk-SK', {ignore: " "})) {
+        res.status(400).send();
+        //console.log("Bad city");
+    }
+    else if (!validator.isPostalCode(postalcode, 'SK')) {
+        res.status(400).send();
+        //console.log("Bad postal");
+    }
+    else {
+        await client 
+            .query("select * from users where email = $1", [email])
+            .then(async dbres => {
+                if (dbres.rowCount > 0) {
+                    res.status(409).send();
+                    return;
+                }
+                await client
+                    .query("insert into users (role, email, password, name, lastname, phone, country, street, city, postalcode) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)",
+                        ['user', email, password, name, lastname, phone, country, street, city, postalcode])
+                    .then(() => {
+                        res.status(200).send();
+                    })
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).send();
+            });
+    }
+});
 
 
 app.get('/api/products', (req, res) => {
@@ -127,8 +252,7 @@ app.get('/api/days/:month', async (req, res) => {
 
 interface Token {
     id: number,
-    role: string,
-    mail: string,
+    email: string,
     iat: number,
     exp: number
 };
@@ -136,10 +260,8 @@ interface Token {
 app.post('/api/submit_order', cookieJwtAuth, async (req, res) => {
 
     // TODO deposit management
-    console.log(req.body);
     const { token, order_deadline, preparation_time, price, details, note } = req.body;
-    const user: Token = jwt.decode(req.body.token) as Token;
-    console.log(user);
+    const user: Token = jwt.decode(token) as Token;
     let currentDate = new Date();
     currentDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate());
     currentDate.setMinutes(currentDate.getMinutes() - currentDate.getTimezoneOffset());
@@ -155,7 +277,6 @@ app.post('/api/submit_order', cookieJwtAuth, async (req, res) => {
     let days: any[] = [];
     let available_wrkld = 0;
     let leftWorkToAssign = preparation_time;
-    console.log(earliestDateToStartProducing, order_deadline);
     await client
         .query("begin")
         .then(async () => {
@@ -164,7 +285,6 @@ app.post('/api/submit_order', cookieJwtAuth, async (req, res) => {
                     [earliestDateToStartProducing, order_deadline])
                 .then(dbres => {
                     days = dbres.rows;
-                    console.log(days);
                     days.map(day => {available_wrkld += day.available_wrkld});
                     if (available_wrkld < preparation_time) throw error("Nedostatok casu na vyrobu");
                 })
@@ -208,14 +328,29 @@ app.post('/api/submit_order', cookieJwtAuth, async (req, res) => {
   
 
 app.post('/api/orders', cookieJwtAuth, (req, res) => {
+    //TODO order by active first
     const user: Token = jwt.decode(req.body.token) as Token
     client
-        .query("select id, status, order_time, order_deadline, deposit_deadline, price, paid, details, note from orders where user_id = $1;", [user.id])
+        .query("select id, status, order_time, order_deadline, deposit_deadline, price, paid, details, note from orders where user_id = $1 order by order_deadline desc;", [user.id])
         .then(dbres => {
             res.status(200).json(dbres.rows);
         })
         .catch(err => {
-            console.error(err);
+            console.log(err);
+            res.status(500).send();
+        })
+})
+
+
+app.post('/api/get_user', cookieJwtAuth, (req, res) => {
+    const user: Token = jwt.decode(req.body.token) as Token
+    client
+        .query("select name, lastname, phone, country, street, city, postalcode from users where email = $1", [user.email])
+        .then(dbres => {
+            res.status(200).json(dbres.rows[0]);
+        })
+        .catch(err => {
+            console.log(err);
             res.status(500).send();
         })
 })
